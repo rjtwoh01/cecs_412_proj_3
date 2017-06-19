2b3cb7158f53b0abe362f798109c40e75d593bf2
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-06-19 15:14:37 -0400

    LCD and Part 3 Set Up

---
+++ Part3/Part2/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\common\components\display\st7565r\st7565r.c
+
+src\ASF\common\services\clock\xmega\sysclk.c
+
+src\ASF\common\services\ioport\xmega\ioport_compat.c
+
+src\ASF\common\services\spi\xmega_usart_spi\usart_spi.c
+
+src\ASF\xmega\boards\xmega_a3bu_xplained\init.c
+
+src\ASF\xmega\drivers\cpu\ccp.s
+
+src\ASF\xmega\drivers\nvm\nvm.c
+
+src\ASF\xmega\drivers\nvm\nvm_asm.s
+
+src\ASF\xmega\drivers\usart\usart.c
+
+src\st7565r_example.c
+
