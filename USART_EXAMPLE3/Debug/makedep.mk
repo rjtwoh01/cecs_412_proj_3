0310e02a2680e45334b6cf5cd0cf6958ec0c9c9b
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-06-19 14:47:01 -0400

    Adds Numbers

---
+++ USART_EXAMPLE3/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\common\services\clock\xmega\sysclk.c
+
+src\ASF\common\services\ioport\xmega\ioport_compat.c
+
+src\ASF\xmega\boards\xmega_a3bu_xplained\init.c
+
+src\ASF\xmega\drivers\cpu\ccp.s
+
+src\ASF\xmega\drivers\nvm\nvm.c
+
+src\ASF\xmega\drivers\nvm\nvm_asm.s
+
+src\ASF\xmega\drivers\usart\usart.c
+
+src\usart_example.c
+
