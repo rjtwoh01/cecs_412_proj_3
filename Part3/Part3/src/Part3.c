a3a401ad57d39251e492b8909891b4cf7280936c
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-06-20 13:46:58 -0400

    Implemented Flash and Wrap Around
    Albiet a work around for the wrap around

--- Part3/Part3/src/Part3.c
+++ Part3/Part3/src/Part3.c
@@ -45,17 +45,18 @@ const int Characters[37][6] = {
 	0x3E, 0x49, 0x49, 0x3E, 0x00,0x00,  //8
 	0x06, 0x09, 0x09, 0x7F, 0x00,0x00,  //9
 	0x7F, 0x41, 0x41, 0x7F, 0x00,0x00,  //0
+	0x00, 0x00, 0x40, 0x00, 0x00, 0x00, //. (USED FOR NEW LINE)
 };
+
+//! the page address to write to
+uint8_t page_address;
+//! the column address, or the X pixel.
+uint8_t column_address;
+//! store the LCD controller start draw line
+uint8_t start_line_address = 0;
 
 int main(void)
 {
-	//! the page address to write to
-	uint8_t page_address;
-	//! the column address, or the X pixel.
-	uint8_t column_address;
-	//! store the LCD controller start draw line
-	uint8_t start_line_address = 0;
-
 	board_init();
 	sysclk_init();
 
@@ -222,6 +223,9 @@ int getCharacter(int input) {
 		case '0':
 		x = 36;
 		break;
+		case '.':
+		x = 37;
+		break;
 		default:
 		break;
 	}
@@ -231,10 +235,20 @@ int getCharacter(int input) {
 void displayCharacter(uint8_t character)
 {
 	// set addresses at beginning of display
+	gpio_set_pin_low(NHD_C12832A1Z_BACKLIGHT); //turns backlight on
+
+	if (character == 37) {
+		start_line_address += 7;
+		st7565r_set_display_start_line_address(start_line_address++);
+		st7565r_set_column_address(0);
+		st7565r_set_page_address(++page_address);
+	}
 
 	int i;
 	for (i = 0; i < 6; i++)
 	{
 		st7565r_write_data(Characters[character][i]);
 	}
+	delay_ms(100);
+	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT); //turns backlight on
 }
