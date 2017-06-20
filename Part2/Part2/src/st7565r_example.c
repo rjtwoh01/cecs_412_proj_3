2e42e9ead55187834ee26dc6d068d879ce5a1e0f
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-06-20 09:54:08 -0400

    Part 2 Implemented
    Untested

--- Part2/Part2/src/st7565r_example.c
+++ Part2/Part2/src/st7565r_example.c
@@ -3,6 +3,17 @@
 #include <st7565r.h>
 #include "led.h"
 
+const int Message[8][6] = {
+	0x3E, 0x41, 0x41, 0x41, 0x00, 0x00, //C
+	0x7F, 0x49, 0x49, 0x49, 0x00, 0x00, //E
+	0x3E, 0x41, 0x41, 0x41, 0x00, 0x00, //C
+	0x46, 0x49, 0x49, 0x49, 0x31, 0x00, //S
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //SPACE
+	0x3C, 0x22, 0x7F, 0x20, 0x00, 0x00, //4
+	0x42, 0x7F, 0x40, 0x00, 0x00, 0x00, //1
+	0x62, 0x51, 0x4D, 0x46, 0x00, 0x00, //2
+};
+
 int main(void)
 {
 	//! the page address to write to
@@ -24,16 +35,25 @@ int main(void)
 	st7565r_set_page_address(0);
 	st7565r_set_column_address(0);
 
-	// fill display with lines
-	for (page_address = 0; page_address <= 4; page_address++) {
-		st7565r_set_page_address(page_address);
-		for (column_address = 0; column_address < 128; column_address++) {
-			st7565r_set_column_address(column_address);
-			/* fill every other pixel in the display. This will produce
-			horizontal lines on the display. */
-			st7565r_write_data(0xAA);
+	//Write message
+	for (int i = 0; i < 8; i++) 
+	{
+		for (int k = 0; k < 6; k++)
+		{
+			st7565r_write_data(Message[i][k]);
 		}
 	}
+
+	// fill display with lines
+	//for (page_address = 0; page_address <= 4; page_address++) {
+		//st7565r_set_page_address(page_address);
+		//for (column_address = 0; column_address < 128; column_address++) {
+			//st7565r_set_column_address(column_address);
+			///* fill every other pixel in the display. This will produce
+			//horizontal lines on the display. */
+			//st7565r_write_data(0xAA);
+		//}
+	//}
 
 	// scroll the display using hardware support in the LCD controller
 	while (true) {
